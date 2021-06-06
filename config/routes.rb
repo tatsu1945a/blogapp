cc6fd2d2124a51d7e472b5b8ecb5a0a579ce5bb8
lmf <vmak8160@gmail.com>
2021-06-06 16:35:28 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root to: 'home#index'
   #get '/' => 'home#index'
-  root to: 'article#index'
+  root to: 'articles#index'
 
 end
