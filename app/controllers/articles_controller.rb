d8af80682e9a9b6be6d31818e64aef60bba339c7
lmf <vmak8160@gmail.com>
2021-06-10 22:33:50 +0900

    a

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,24 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
  
+  def new
+    @article = Article.new    
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: 'ほぞんできた'
+    else
+      flash.now[:error] = '保存だめ'
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
 
